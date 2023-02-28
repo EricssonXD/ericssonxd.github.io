26d91773bf9d885d6a202a03cfc4f250b2cf4637
EricssonXD <114271127+EricssonXD@users.noreply.github.com>
2023-02-28 17:24:29 +0800

    icon update for darkmode

--- lib/include/navbar/logo/navbarlogo.dart
+++ lib/include/navbar/logo/navbarlogo.dart
@@ -16,48 +16,26 @@ class NavbarLogo extends StatelessWidget {
   }
 }
 
-class NavbarLogoMob extends StatelessWidget {
-  const NavbarLogoMob({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const SizedBox(
-        height: 80,
-        width: 80,
-        child: Image(
-          alignment: Alignment.bottomLeft,
-          image: AssetImage('assets/icons/icon.png'),
-        )
-        // Text(
-        //   'Ericsson Chau',
-        //   style: TextStyle(
-        //       color: Colors.blueAccent, fontSize: 20, fontFamily: 'Agustina'),
-        // )
-        );
-  }
-}
-
 class NavbarLogoDesk extends StatelessWidget {
   const NavbarLogoDesk({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.only(top: 5.0),
-      child: SizedBox(
-          height: 80,
-          width: 80,
-          child: Image(
-            alignment: Alignment.bottomLeft,
-            image: AssetImage('assets/icons/icon.png'),
-          )
-          // Text(
-          //   'Ericsson Chau',
-          //   style: TextStyle(
-          //       color: Colors.blueAccent, fontSize: 30, fontFamily: 'Agustina'),
-          // ),
-          // //Image.asset('assets/images/logo.png'),
-          ),
+    return const Image(
+      alignment: Alignment.bottomLeft,
+      image: AssetImage('assets/icons/icon2.png'),
+    );
+  }
+}
+
+class NavbarLogoMob extends StatelessWidget {
+  const NavbarLogoMob({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Image(
+      alignment: Alignment.bottomLeft,
+      image: AssetImage('assets/icons/icon2.png'),
     );
   }
 }
